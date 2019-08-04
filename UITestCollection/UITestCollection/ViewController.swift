b343d3f9c5d9d5d2f825a7033b8c6f9fcc4a4b7f
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-08-04 12:34:20 +0300

    Created new project

---
+++ UITestCollection/UITestCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITestCollection
+//
+//  Created by yauheni prakapenka on 04/08/2019.
+//  Copyright © 2019 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
