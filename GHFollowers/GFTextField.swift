0aea6aadc6652400a3934123674f6301ad906dda
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-01-08 11:21:05 +0300

    Created custom button

---
+++ GHFollowers/GFTextField.swift
@@ -0,0 +1,9 @@
+//
+//  GFTextField.swift
+//  GHFollowers
+//
+//  Created by yauheni prakapenka on 08.01.2020.
+//  Copyright © 2020 yauheni prakapenka. All rights reserved.
+//
+
+import Foundation
