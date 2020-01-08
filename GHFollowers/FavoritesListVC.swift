b1b3ab137e7a927a90cf836effeca8d1c3db815b
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-01-08 10:17:22 +0300

    Created tabbar

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by yauheni prakapenka on 08.01.2020.
+//  Copyright © 2020 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+}
--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -1,21 +1,18 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GHFollowers
 //
-//  Created by yauheni prakapenka on 07.01.2020.
+//  Created by yauheni prakapenka on 08.01.2020.
 //  Copyright © 2020 yauheni prakapenka. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchVC: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .systemPink
     }
-
-
 }
-
