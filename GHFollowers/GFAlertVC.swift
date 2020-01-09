7736199ba3b060558cba8709006b663dd7153b49
Yauheni Prakapenka <eprakopenko@intervale.ru>
2020-01-09 21:13:02 +0300

    Created Custom Alert

---
+++ GHFollowers/GFAlertVC.swift
@@ -0,0 +1,68 @@
+//
+//  GFAlertVC.swift
+//  GHFollowers
+//
+//  Created by yauheni prakapenka on 08.01.2020.
+//  Copyright © 2020 yauheni prakapenka. All rights reserved.
+//
+
+import UIKit
+
+class GFAlertVC: UIViewController {
+    
+    let containerView = UIView()
+    let titleLabel = GFTitleLabel(texAligment: .center, fontSize: 20)
+    let messsageTitle = GFBodyLabel(texAligment: .center)
+    let actionButton = GFButton(backgroundColor: .systemPink, title: "Ok")
+    
+    var alertTitle: String?
+    var message: String?
+    var buttonTitle: String?
+    
+    init(title: String, message: String, buttonTitle: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle = title
+        self.message = message
+        self.buttonTitle = buttonTitle
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        configureContainerView()
+        configureTitleLabel()
+    }
+    
+    func configureContainerView() {
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.layer.cornerRadius = 16
+        containerView.layer.borderWidth = 2
+        containerView.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 280),
+            containerView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+    }
+    
+    func configureTitleLabel() {
+        containerView.addSubview(titleLabel)
+        titleLabel.text = alertTitle ?? "Something went wrong"
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -20),
+            titleLabel.heightAnchor.constraint(equalToConstant: 28)
+        ])
+    }
+    
+}
